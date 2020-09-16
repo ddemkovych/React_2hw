0bf1e832e95dd97dfe73bcf534b2f2d13d6c47f0
Dmytro <ddemkovych4@gmail.com>
2020-09-16 22:00:55 +0300

    Updates

---
+++ precache-manifest.74e8143c5c181b944e11146d2f91f2f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f331e19be6bee4ecb178db2831dfcb0",
+    "url": "/React_2hw/index.html"
+  },
+  {
+    "revision": "440245be88cf05eb98cf",
+    "url": "/React_2hw/static/css/main.d9c84606.chunk.css"
+  },
+  {
+    "revision": "5807135e140e7a72dacf",
+    "url": "/React_2hw/static/js/2.98454d88.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_2hw/static/js/2.98454d88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "440245be88cf05eb98cf",
+    "url": "/React_2hw/static/js/main.2abab2a9.chunk.js"
+  },
+  {
+    "revision": "064f7ad7b04811ad8b82",
+    "url": "/React_2hw/static/js/runtime-main.ae85b00e.js"
+  },
+  {
+    "revision": "18c2bd0f0ef487a4d7c0ca312c97180b",
+    "url": "/React_2hw/static/media/unknown.18c2bd0f.jpg"
+  }
+]);
